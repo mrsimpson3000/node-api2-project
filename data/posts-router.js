b4a5aaa5fc4e523db1ad249be3377d3e4a8313d6
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-05 18:51:29 -0400

    Two GET requests up and running.

--- data/posts-router.js
+++ data/posts-router.js
@@ -17,4 +17,23 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
 module.exports = router;
